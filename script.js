7cd7f8a7ec0c443a475da8698db705c9e300c4be
amanzaheer <amanzaheer0808@gmail.com>
2025-07-10 10:39:23 +0500

    push

---
+++ script.js
@@ -0,0 +1,11 @@
+// import { write } from 'fs';
+
+
+// write("Hello.jsx","idhr text ai ga",function(err){
+  
+//     if (err)
+//         console.error(err)
+//     else
+//     console.log("done");
+
+// } )
